b741290b012fb58c9c7c08d71f5e7c03ac106b7f
zoesant <zoesant941@gmail.com>
2022-06-02 18:27:16 -0300

    first commit

---
+++ ejs_views/views/datos.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        Titulo
+    </head>
+    <body>
+        
+
+        <form action="views/datos" method="post">
+            <input type="text" name="nombre"/>
+            <button type="submit"> Enviar datos</button>
+        </form>
+    </body>
+</html>
